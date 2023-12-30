07138e9df32de4f9bcd74be431bf46687a78e1e0
suraj-developer <145120352+SJ563@users.noreply.github.com>
2023-12-29 21:49:14 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function namecaller()
+{
+var fb = document.getElementById('first-box').value;
+var sb = document.getElementById('second-box').value;
+var tb = document.getElementById('third-box').value;
+document.getElementById('fourth-box').value = fb+" "+sb+" "+" "+tb;
+}
